b70772d5a77c8604a2857cca208769fcac63aa07
hjd <hjd1622@qq.com>
2019-07-24 10:15:09 +0800

    0000000000000000000000000123213213

--- src/e.java
+++ src/e.java
@@ -3,8 +3,14 @@
  */
 public class e {
     public static void main(String[] args) {
-        b vs = new b("23");
+        String ss = "12121155822826D3E19437482543E9624BEB1CDADA27071AEA5B597ED7607D4E94F7C8833029B306976E2D0F8B350FBEDA4E332556E549ED3568964235B586CADFFFA2B5FE758D4682EB4997BB5F32D5CCE7BC93F34D4B47F67E8CAEEA7F98DBD42B4E";
+        String s1 = "55822826D3E19437482543E9624BEB1CDADA27071AEA5B597ED7607D4E94F7C8833029B306976E2D0F8B350FBEDA4E332556E549ED3568964235B586CADFFFA2B5FE758D4682EB4997BB5F32D5CCE7BC93F34D4B47F67E8CAEEA7F98DBD42B4E";
+        int i = ss.lastIndexOf("-");
+//        System.out.println(ss.substring(0,i));
+//        System.out.println(ss.endsWith("RSAD!12"));
+//        System.out.println(ss.substring(ss.indexOf("-")+1));
+//        System.out.println(ss.substring(0,ss.indexOf(".")));
 
-        System.out.pri2131312312312312312ntln((false ==false));
+        System.out.println(ss.indexOf(s1));
     }
 }
