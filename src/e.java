4c528cf5033c63678a3835a7dfc4f816912234d3
hjd <hjd1622@qq.com>
2019-08-06 09:54:27 +0800

    2222222

--- src/e.java
+++ src/e.java
@@ -11,7 +11,8 @@ public class e {
 //        System.out.println(ss.substring(ss.indexOf("-")+1));
 //        System.out.println(ss.substring(0,ss.indexOf(".")));
 
-        System.out.println(ss.indexOf(s1) );qweqweq
+        System.out.println(ss.indexOf(s1) );
+        21321323
     }
 }
 21321321
