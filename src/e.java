de9b871afba8ad5e15a6f32cd11a0cc56a54e51c
hjd <hjd1622@qq.com>
2019-08-06 09:54:15 +0800

    2222222

--- src/e.java
+++ src/e.java
@@ -11,7 +11,7 @@ public class e {
 //        System.out.println(ss.substring(ss.indexOf("-")+1));
 //        System.out.println(ss.substring(0,ss.indexOf(".")));
 
-        System.out.println(ss.indexOf(s1) );
+        System.out.println(ss.indexOf(s1) );qweqweq
     }
 }
 21321321
