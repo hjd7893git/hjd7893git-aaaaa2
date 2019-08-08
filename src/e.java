d3441e0970f6d09bb9ea313738fdf7fd6b9bd747
hjd <hjd1622@qq.com>
2019-08-08 15:21:21 +0800

    域名访问socket请求

--- src/e.java
+++ src/e.java
@@ -1,18 +1,24 @@
+import java.io.*;
+import java.net.Socket;
+
+import static java.lang.System.in;
+
 /**
  * Created by Administrator on 2019/6/11.
  */
 public class e {
-    public static void main(String[] args) {
-        String ss = "12121155822826D3E19437482543E9624BEB1CDADA27071AEA5B597ED7607D4E94F7C8833029B306976E2D0F8B350FBEDA4E332556E549ED3568964235B586CADFFFA2B5FE758D4682EB4997BB5F32D5CCE7BC93F34D4B47F67E8CAEEA7F98DBD42B4E";
-        String s1 = "55822826D3E19437482543E9624BEB1CDADA27071AEA5B597ED7607D4E94F7C8833029B306976E2D0F8B350FBEDA4E332556E549ED3568964235B586CADFFFA2B5FE758D4682EB4997BB5F32D5CCE7BC93F34D4B47F67E8CAEEA7F98DBD42B4E";
-        int i = ss.lastIndexOf("-");
-//        System.out.println(ss.substring(0,i));
-//        System.out.println(ss.endsWith("RSAD!12"));
-//        System.out.println(ss.substring(ss.indexOf("-")+1));
-//        System.out.println(ss.substring(0,ss.indexOf(".")));
-
-        System.out.println(ss.indexOf(s1) );
-        21321323
+    public static void main(String[] args) throws IOException {
+        String mesg = "0001793f6abfcf-3af1-11e8-8f91-507b9ddd8bdc07console02A02{\"keySchema\":\"Y\",\"ZMKSchema\":\"Y\",\"keyValue\":\"YDFCCB1E0C884682181542E90EE91F2AFBE2AB5FFCFE1981E\",\"keyCode\":\"000\",\"createMode\":\"1\"}";
+        Socket socket = new Socket("www.hxtc.com", 21201);
+        OutputStream out = socket.getOutputStream();
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out.write(mesg.getBytes());
+        String str = null;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
+        out.close();
+        out.flush();
+        br.close();
     }
-}1111111111111111
-111111111111
+}
