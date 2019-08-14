00bf490bb028ea349a6bce6f11edc49963b81f38
hjd <hjd1622@qq.com>
2019-08-14 17:30:25 +0800

    域名访问socket请求

--- src/b.java
+++ src/b.java
@@ -1,19 +1,24 @@
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by Administrator on 2019/6/17.
  */
 public final class b {
-    private String a;
-    b (String a){
-        this.a =a;
-    }
+    public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("www.hxtc.com", 21201);
+        PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
+        pw.println("GET /1.html  / HTTP/1.1");
+        pw.println("Accept: */*");
+        pw.println("Host: localhost:8888");
+        pw.println("Connection: Keep-Alive");
+        pw.println("");
 
-    qeqwe
-    qeqweewq
-            w
-    eqw
+        BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String str = null;
+        while ((str = br.readLine()) != null) {
+            System.out.println(str);
+        }
 
-    public void setEqw(w eqw) {
-        eqweqw
-                e
     }
 }
